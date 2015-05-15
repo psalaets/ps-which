773b861f1d97ef1522b9f09d2a3c61f551c9664e
Paul Salaets <psalaets@gmail.com>
2015-05-15 18:31:58 -0400

    sort of making a mess

--- karma-info.conf.js
+++ karma-info.conf.js
@@ -2,5 +2,6 @@ var baseConfig = require('./karma-base.conf');
 
 module.exports = function(config) {
   baseConfig.files.splice(1, 0, 'test/info-test.js');
+  baseConfig.logLevel = config.LOG_INFO;
   config.set(baseConfig);
 };
